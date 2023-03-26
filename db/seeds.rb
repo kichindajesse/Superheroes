aa19fed98f1f3b7575c6d5d5dcad330d19290f04
kichinda <53686544+kichinda@users.noreply.github.com>
2023-03-26 20:06:06 +0300

    third commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-# Heroes
-# Heroes
-Hero.create(name: 'Kamala Khan', super_name: 'Ms. Marvel')
-Hero.create(name: 'Doreen Green', super_name: 'Squirrel Girl')
-Hero.create(name: 'Gwen Stacy', super_name: 'Spider-Gwen')
 
-# Powers
-Power.create(name: 'Super Strength', description: 'Gives the wielder super-human strength')
-Power.create(name: 'Flight', description: 'Gives the wielder the ability to fly through the skies at supersonic speed')
-Power.create(name: 'Invisibility', description: 'Allows the wielder to become invisible at will')
+ puts "start seeding"
+# create some powers
+power1 = Power.create(name: "Flight", description: "Ability to fly")
+power2 = Power.create(name: "Super strength", description: "Unmatched physical strength")
+power3 = Power.create(name: "Invisibility", description: "Ability to become invisible")
 
-# Hero Powers
-HeroPower.create(hero_id: 1, power_id: 1, strength: 'Strong')
-HeroPower.create(hero_id: 1, power_id: 2, strength: 'Average')
-HeroPower.create(hero_id: 2, power_id: 3, strength: 'Weak')
+# create some heroes
+hero1 = Hero.create(name: "Clark Kent", super_name: "Superman")
+hero2 = Hero.create(name: "Diana Prince", super_name: "Wonder Woman")
+hero3 = Hero.create(name: "Bruce Wayne", super_name: "Batman")
 
+# assign some powers to heroes
+HeroPower.create(strength: "10", hero: hero1, power: power1)
+HeroPower.create(strength: "8", hero: hero1, power: power2)
+HeroPower.create(strength: "6", hero: hero2, power: power2)
+HeroPower.create(strength: "9", hero: hero2, power: power3)
+HeroPower.create(strength: "7", hero: hero3, power: power1)
+HeroPower.create(strength: "5", hero: hero3, power: power3)
+
+puts "d seeding"
