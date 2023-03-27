0638f4d048692a60f2bb25d2d22da15c4b3bd796
kichinda <53686544+kichinda@users.noreply.github.com>
2023-03-27 10:55:24 +0300

    done

--- app/controllers/hero_powers_controller.rb
+++ app/controllers/hero_powers_controller.rb
@@ -1,16 +1,38 @@
 class HeroPowersController < ApplicationController
+
   def create
     hero_power = HeroPower.new(hero_power_params)
     if hero_power.save
       render json: hero_power, status: :created
     else
-      render json: { errors: hero_power.errors }, status: :unprocessable_entity
+      render json: { error: hero_power.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+  
+  def show
+    power = Power.find_by(id: params[:id])
+
+    if power
+      render json: power.as_json(only: [:id, :name, :description])
+    else
+      render json: { error: "Power not found" }, status: :not_found
+    end
+  end
+
+  def update
+    power = Power.find_by(id: params[:id])
+
+    if power
+      power.update(power_params)
+      render json: power.as_json(only: [:id, :name, :description])
+    else
+      render json: { error: "Power not found" }, status: :not_found
     end
   end
 
   private
 
-  def hero_power_params
-    params.require(:hero_power).permit(:hero_id, :power_id)
+  def power_params
+    params.permit(:description)
   end
 end
